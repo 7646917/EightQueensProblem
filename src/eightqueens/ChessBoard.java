1ccc97d563d4bd6685073bfd1f890ad8f9a1c8a2
7646917 <7646917@student.swin.edu.au>
2016-10-07 14:03:22 +1100

    Setup logic for finding the position

--- src/eightqueens/ChessBoard.java
+++ src/eightqueens/ChessBoard.java
@@ -12,19 +12,42 @@ package eightqueens;
 public class ChessBoard {
 
     private final Tile[][] tiles;
+    private final Queen[] queens;
 
-    public ChessBoard(int size) {
+    public ChessBoard(int size, Queen[] queens) {
         tiles = new Tile[size][size];
+        this.queens = queens;
     }
 
     public void solveProblem() {
         System.out.println("Solving Eight Queens Problem");
         System.out.println("Size of board is " + getSize());
+
+        System.out.println("A possible solution is: ");
+
+        for (String s : checkPosition()) {
+            System.out.println("Queen at: " + s);
+        }
     }
-    
-    public boolean checkPosition(Queen[] queens){
+
+    public String[] checkPosition() {
         //For every queen, check if
-        return false;
+        String[] result = new String[8];
+        
+        /*
+    If 
+        all queens are placed return true and print the solution matrix.
+    Else
+        Try all the rows in the cur­rent column.
+        Check if queen can be placed here safely if yes mark the current cell in solu­tion matrix as 1 and try to solve the rest of the problem recursively.
+        If placing the queen in above step leads to the solu­tion return true.
+        If placing the queen in above step does not lead to the solu­tion , BACKTRACK, mark the current cell in solu­tion matrix as 0 and return false.
+        If all the rows are tried and noth­ing worked, return false and print NO SOLUTION
+        
+         */
+        
+
+        return result;
     }
 
     public Tile[][] getTiles() {
